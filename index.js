e522772474811eb3a46bcb3d3cba998aeb21a58e
Terka Codes <107133029+terka-codes@users.noreply.github.com>
2023-03-26 22:28:52 +0200

    like or nope badge update

--- index.js
+++ index.js
@@ -2,12 +2,27 @@ import {Dogs} from "./Dog.js"
 import {dogs} from "./data.js"
 
 let dog = new Dogs(dogs[0])
-document.getElementById("main-img").innerHTML = dog.getDogsHtml()
+render()
 
-document.getElementById("nope-btn").addEventListener("click", () => {
-    dog.this.hasBeenSwiped ? false : true
+function render() {
+    document.getElementById("main-img").innerHTML = dog.getDogsHtml()
+}
+
+// TODO: Work on function that will change the dog object to the next one after the user clicks on the like or nope button but set the timeout function to 1.5 seconds so that the user can see the like or nope badge before the next dog appears or change their mind and click on the other button
+
+function adressRateBtnClick(whatsClicked, oppositeOne) {
+    if (dog[oppositeOne]) {
+        dog[oppositeOne] = false
+    }
+
+    dog[whatsClicked] = !dog[whatsClicked]
+    render()
+}
+
+document.getElementById("nope-btn").addEventListener("click", function() {
+    adressRateBtnClick("hasBeenSwiped", "hasBeenLiked")
 })
 
-document.getElementById("like-btn").addEventListener("click", () => {
-    dog.this.hasBeenLiked ? false : true
+document.getElementById("like-btn").addEventListener("click", function() {
+    adressRateBtnClick("hasBeenLiked", "hasBeenSwiped")
 })
