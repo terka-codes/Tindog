a45bc1aa52dfa6c59f6c227c3ba055fac8313c85
Terka Codes <107133029+terka-codes@users.noreply.github.com>
2023-03-27 09:35:50 +0200

    code comments added

--- Dog.js
+++ Dog.js
@@ -1,14 +1,23 @@
 class Dogs {
     constructor(data) {
+        // set the properties of the new dog object to match the properties of the provided data
         Object.assign(this, data)
     }
     getDogsHtml() {
+        // extract the properties we need from the current dog object
         const {name, avatar, age, bio, hasBeenSwiped, hasBeenLiked } = this
 
-        const liked = this.hasBeenLiked ? `<img class="status-img" src="images/like-image.png" alt="Like badge">` : ""
-        const swiped = this.hasBeenSwiped ? `<img class="status-img" src="images/nope-image.png" alt="Nope badge">` : ""
+        // create a "Like" badge if the dog has been liked
+        const liked = this.hasBeenLiked ? 
+            `<img class="status-img" src="images/like-image.png" alt="Like badge">` 
+            : ""
+            // create a "Nope" badge if the dog has been swiped
+        const swiped = this.hasBeenSwiped ? 
+            `<img class="status-img" src="images/nope-image.png" alt="Nope badge">` 
+            : ""
         
-        const dogHtml = `
+        // create the HTML for the dog photo card using the extracted properties and the created badges
+        return dogHtml = `
             <div class="dog-photo-card-div">
                 <img class="dog-photo-card" src="${avatar}">
                 ${liked}
@@ -18,15 +27,7 @@ class Dogs {
                     <p>${bio}</p>
                 </div>
             </div>`
-        
-        return dogHtml
     }
 }
-/*  name: "Rex",
-    avatar: "images/dog-rex.jpg",
-    age: 25,
-    bio: "Art. Literature. Natural wine. Yoga.",
-    hasBeenSwiped: false,
-    hasBeenLiked: false */
 
 export {Dogs}
--- index.js
+++ index.js
@@ -1,51 +1,61 @@
 import {Dogs} from "./Dog.js"
 import {dogs} from "./data.js"
 
+// Keep track of the current dog being shown
 let dogsCounter = 0
-
 let dog = new Dogs(dogs[dogsCounter])
+
+// Render the initial dog card
 render()
 
+// Show the next dog when the user clicks either "Nope" or "Like"
 function addDog() {
+    // Check if there are any dogs left to show
     if (dogsCounter < dogs.length - 1) {
         dogsCounter++
         dog = new Dogs(dogs[dogsCounter])
         render()
+        // Remove the "active" class from both buttons
         document.getElementById("nope-btn").classList.remove("active")
         document.getElementById("like-btn").classList.remove("active")
     } else {
+        // If there are no more dogs left, show an alert message and remove the buttons
         document.getElementById("main-img").innerHTML = `
             <h1 class="alert-no-more-dogs">There are no more dogs to show you 🐶</h1>`
-            document.getElementById("rate-btns").innerHTML = ""
+        document.getElementById("rate-btns").innerHTML = ""
     }
 }
 
+// Render the current dog image
 function render() {
     document.getElementById("main-img").innerHTML = dog.getDogsHtml()
 }
 
-// TODO: Work on function that will change the dog object to the next one after the user clicks on the like or nope button but set the timeout function to 1.5 seconds so that the user can see the like or nope badge before the next dog appears or change their mind and click on the other button
-
-
-
-function adressRateBtnClick(whatsClicked, oppositeOne, button, oppositeButton) {
+// Handle when the user clicks either "Nope" or "Like"
+function handleRateBtnClick(whatsClicked, oppositeOne, button, oppositeButton) {
+    // If the opposite button was clicked before, reset its state
     if (dog[oppositeOne]) {
         dog[oppositeOne] = false
     }
 
+    // Toggle the state of the clicked button
     dog[whatsClicked] = !dog[whatsClicked]
     render()
 
+    // Toggle the "active" class for the clicked button and remove it from the opposite button
     document.getElementById(`${button}`).classList.toggle("active");
     document.getElementById(`${oppositeButton}`).classList.remove("active")
 
+    // Show the next dog after 1.5 seconds
     setTimeout(() => addDog(), 1500)
 }
 
+// Handle when the "Nope" button is clicked
 document.getElementById("nope-btn").addEventListener("click", function() {
-    adressRateBtnClick("hasBeenSwiped", "hasBeenLiked", "nope-btn", "like-btn")
+    handleRateBtnClick("hasBeenSwiped", "hasBeenLiked", "nope-btn", "like-btn")
 })
 
+// Handle when the "Like" button is clicked
 document.getElementById("like-btn").addEventListener("click", function() {
-    adressRateBtnClick("hasBeenLiked", "hasBeenSwiped", "like-btn", "nope-btn")
+    handleRateBtnClick("hasBeenLiked", "hasBeenSwiped", "like-btn", "nope-btn")
 })
